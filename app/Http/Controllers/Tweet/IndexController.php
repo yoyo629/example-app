585e00cb40985b0b8da9a730bdbe2a642409c975
Yosuke <king.crimson.723.yu@gmail.com>
2025-03-14 23:50:07 +0900

    add:つぶやき保存用テーブル作成

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -15,6 +15,6 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return 'Single Action!';
+        return view('tweet.index', ['name' => 'laravel']);
     }
 }
