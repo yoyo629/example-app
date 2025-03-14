63530a7f30acc51e3b6729a4da937c051d35dfe6
Yosuke <king.crimson.723.yu@gmail.com>
2025-03-14 23:10:29 +0900

    add:つぶやき用のコントローラー新規作成

--- app/Http/Controllers/Sample/IndexController.php
+++ app/Http/Controllers/Sample/IndexController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
-}
+    public function show()
+    {
+        return 'Hello';
+    }
+
+    public function showId($id)
+    {
+        return "Hello {$id}";
+    }
+};
