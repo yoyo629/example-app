24be5221aa8c47da81acd57e353e8bb288460c74
Yosuke <king.crimson.723.yu@gmail.com>
2025-04-06 02:25:51 +0900

    add:つぶやき削除処理追加

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
 Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
 Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+
