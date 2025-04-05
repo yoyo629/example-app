69ca3e612621ef85018694b1a621cdf92c42628b
Yosuke <king.crimson.723.yu@gmail.com>
2025-04-06 02:55:49 +0900

    add:breezインストール

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Tweet\CreateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,12 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
-Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showId']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
-Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
-Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
-Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
-Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
-
+require __DIR__.'/auth.php';
