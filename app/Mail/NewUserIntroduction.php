7ecbb98b1190f8609f1e9e8bc9706384daf6dddc
Yosuke <king.crimson.723.yu@gmail.com>
2025-05-22 01:02:03 +0900

    add:メール機能追加

---
+++ app/Mail/NewUserIntroduction.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Content;
+
+class NewUserIntroduction extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subject = '新しいユーザーが追加されました！';
+    public User $toUser;
+    public User $newUser;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(User $toUser, User $newUser)
+    {
+        $this->toUser = $toUser;
+        $this->newUser = $newUser;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'email.new_user_introduction',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
