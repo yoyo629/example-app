22d49d20feb2ce449c8c7d58a1216b411753c1c5
Yosuke <king.crimson.723.yu@gmail.com>
2025-06-15 22:43:04 +0900

    fix:cloudinary.php修正

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'cloud_name'  => env('CLOUDINARY_CLOUD_NAME', null),
-    'api_key'  => env('CLOUDINARY_API_KEY', null),
-    'api_secret'  => env('CLOUDINARY_API_SECRET', null)
+    'cloud_name' => env('CLOUDINARY_CLOUD_NAME', null),
+    'api_key' => env('CLOUDINARY_API_KEY', null),
+    'api_secret' => env('CLOUDINARY_API_SECRET', null),
 ];
